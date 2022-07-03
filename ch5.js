1481bcddfb09f5dad1f43efc9acf72faaa3f310d
Ikramah <92651495+IkramahAhmed@users.noreply.github.com>
2022-07-03 06:30:52 -0700

    Add files via upload

---
+++ ch5.js
@@ -0,0 +1,68 @@
+// 8. Write a program to store 3 student names in an array.Take
+// another array to store score of these three students.
+// Assume that total marks are 500 for each student, display
+// the scores & percentages of students like:
+
+// var a=["ikramah","ali","omar"];
+// var b=[320,230,480];
+// for(i=0;i<a.length;i++){
+//     var total= ((b[i]/500)*100);
+// console.log("score of" + a[i] +  "is" ,b[i], total + "%" );
+//     }
+
+// 7. Write a program to implement checkout process of a 
+// shopping cart system for an e-commerce website. Store 
+// the following in variables
+// MATH EXPRESSIONS | JAVASCRIPT
+
+// a. Price of item 1
+// b. Price of item 2
+// c. Ordered quantity of item 1
+// d. Ordered Quantity of item 2
+// e. Shipping charges
+// Compute the total cost & show the receipt in your browser.
+
+// var items1=650;
+// var quantity=3;
+// var items2=100;
+// var quantity2=7;
+// var result1=(items1*3);
+// var result2=(items2*7);
+// var finalresult=result1+ result2;
+
+
+// console.log("price of item 1 is", items1);
+// console.log("Quantity of item 1 is", quantity);
+
+// console.log("price of item 2 is", items2);
+// console.log("Quantity of item 2 is", quantity2);
+
+// console.log("Total cost of your order is", finalresult);
+
+// 8. Store total marks & marks obtained by a student in 2
+// variables. Compute the percentage & show the result in
+// your browser
+
+
+// var total = 980;
+// var english=430;
+// var urdu=230;
+// var percentage=(((english+urdu)/980)*100);
+
+// console.log("total is",total);
+// console.log("Marks Obtained",english+urdu);
+// console.log("percentage" + percentage + "%");
+
+// Assume we have 10 US dollars & 25 Saudi Riyals. Write a
+// script to convert the total currency to Pakistani Rupees.
+// Perform all calculations in a single expression.
+// (Exchange rates : 1 US Dollar = 104.80 Pakistani Rupee
+// and 1 Saudi Riyal = 28 Pakistani Rupee)
+
+// var dollar = 104.80;
+// var riyal = 28;
+
+// result=((dollar*10)+(riyal*28));
+
+// console.log("Total currency in PKR", result + "rupees"  );
+
